6488b170914a7d4e1a7614a5621ec8a3ab84cd9f
cj05 <cjcoolsuper@gmail.com>
2023-11-08 10:57:58 +0700

    e

---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+class CustomButton:
+    def __init__(self,root,text,posx,posy):
+        self.root = tk.Button(root.root,text=text)
+        self.root.bind("<Button-1>",lambda s : root.ButtonHandler(s,text))
+        self.root.place(x=posx,y=posy)
+        self.root.pack()
+    
+
+class Calculator:
+    def ButtonHandler(self,event,text):
+        print(event)
+    def __init__(self):
+            self.root = tk.Tk()
+            self.root.geometry("300x300")
+            self.root.title("Calculator")
+            CustomButton(self,"1",-150,1)
+            self.root.mainloop()
+
+
+Calculator()
